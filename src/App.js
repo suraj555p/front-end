4d6bcbf64d012238b7c954201acc24a2757d8bed
suraj <suraj87parmar@gmail.com>
2024-11-09 19:07:50 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation.js';
+import Home from './components/Home.js';
+import Footer from './components/Footer.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Orders from './components/Orders.js';
+import BookingForm from './Services/Bookingform.js';
+import BookingStatus from './Services/Bookingstatus.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path='/booking' element={<BookingForm/>}/>
+        <Route path='/booking-status' element={<BookingStatus/>}/>
+      </Routes>
+      <Footer/>
+   </Router>
   );
 }
 
